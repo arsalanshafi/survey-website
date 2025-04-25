4ccfeac85b860c70fb0685e1e439f4239ef0f8ae
abuArsalan <arsalanshafi911@gmail.com>
2025-04-25 15:32:15 +0530

    created a simple form

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,13 @@
-console.log("hey there");
+const express = require("express");
+
+const app = express();
+
+
+app.use(express.static("./public"));
+
+
+
+
+
+
+app.listen(5000,()=>console.log("app is listening on 5000"));
