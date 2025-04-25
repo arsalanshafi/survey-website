8fdc2ae7ae6a57b9ae1bdaf6f6c584df0c9aa2b4
abuArsalan <arsalanshafi911@gmail.com>
2025-04-25 15:55:52 +0530

    data gets written to a text file

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,22 @@
 const express = require("express");
+const { writeFileSync } = require("fs")
+const path = require("path");
 
 const app = express();
 
 
 app.use(express.static("./public"));
+app.use(express.urlencoded({extended:false}))
 
+const filePath = path.join(__dirname,"/data.txt");
+const resFilePath = path.join(__dirname,"/response.html")
+
+app.post("/survey",(req,res)=>{
+    console.log(req.body);
+    const {name,address,age} = req.body
+    writeFileSync(filePath,`\n name : ${name} , address:${address} , age:${age}`,{flag:"a"})
+    res.sendFile(resFilePath);
+})
 
 
 
